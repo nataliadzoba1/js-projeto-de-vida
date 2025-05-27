d645bcf65d36d6c6d7862153f3f86208f38880df
natalia dzoba <145125394+nataliadzoba1@users.noreply.github.com>
2025-05-27 10:50:11 +0000

    contadpr

--- main.js
+++ main.js
@@ -13,13 +13,39 @@ for (let i = 0; i < botoes.length; i++){
     }
 }
 
-const contadores  = document.querySelectorAll(".contador");
+const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo1 = new Date("2025-10-05T00:00:00");
+const tempoObjetivo2 = new Date("2025-12-05T00:00:00");
+const tempoObjetivo3 = new Date("2025-12-05T00:00:00");
+const tempoObjetivo4 = new Date("2026-01-05T00:00:00");
+
+const tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4];
 
 contadores[0].textContent = calculaTempo(tempoObjetivo1);
 
 function calculaTempo(tempoObjetivo){
     let tempoAtual = new Date();
-    let tempoFinal = tempoObjetivo - tempoAtual;
-    
+    let tempoFinal =  tempoObjetivo - tempoAtual;
+    let segundos = Math.floor(tempoFinal/1000);
+    let minutos = Math.floor(segundos/60);
+    let horas = Math.floor(minutos/60);
+    let dias = Math.floor(horas/24);
+
+    segundos %= 60;
+    minutos %= 60;
+    horas %= 24;
+
+    return dias + " dias "+ horas + " horas " + minutos + " minutos "+ segundos + " Segundos";
 }
+
+function atualizarCronometro(){
+    for(let i = 0; i < contadores.length; i++){
+        contadores[i].textContent = calculaTempo(tempos[i]);
+    } 
+}
+function comecaCronometro(){
+    atualizarCronometro();
+    setInterval(atualizarCronometro, 1000);
+}
+comecaCronometro();
+
